ee7839873a7839ba02cbb0161bd3e14903b3ea18
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-27 00:40:31 +0330

    add basic prompt with commands

--- lib/robots_dilemma.rb
+++ lib/robots_dilemma.rb
@@ -3,10 +3,33 @@ require "tty-prompt"
 require_relative "robots_dilemma/version"
 require_relative "robots_dilemma/robot"
 
-
 module RobotsDilemma
   class Error < StandardError; end
-  # Your code goes here...
+
+  prompt = TTY::Prompt.new
   robot = RobotsDilemma::Robot.new
-  robot.report
+
+  running = true
+  while running
+    command = prompt.ask
+    #TODO encapsulate this part for testing and other forms of input
+    command_parts = command.split(" ")
+    case command_parts[0]
+    when "PLACE"
+      position = command_parts[1].split(',')
+      robot.place(position[0].to_i, position[1].to_i, position[2])
+    when "MOVE"
+
+    when "RIGHT"
+
+    when "LEFT"
+
+    when "REPORT"
+      robot.report
+    when "exit"
+      running = false
+    else
+      puts 'invalid command'
+    end
+  end
 end
