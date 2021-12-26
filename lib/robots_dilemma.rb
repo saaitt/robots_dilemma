27e20b2b7ed187d1f68d0adbf18bccb8c0a5d5a8
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-27 00:13:35 +0330

    initiate project skeleton

---
+++ lib/robots_dilemma.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "robots_dilemma/version"
+
+
+module RobotsDilemma
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "robots_dilemma"
+
+require "minitest/autorun"
