cb4d91180abc424de2d4783d619b26df20430774
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-27 00:31:11 +0330

    add move basic

--- lib/robots_dilemma/robot.rb
+++ lib/robots_dilemma/robot.rb
@@ -6,6 +6,21 @@ module RobotsDilemma
       @facing = "NORTH"
     end
 
+    def move
+      case @facing
+      when 'NORTH' #TODO: check edge cases
+        @position_y += 1
+      when 'SOUTH'
+        @position_y -= 1
+      when 'EAST'
+        @position_x += 1
+      when 'WEST'
+        @position_x -= 1
+      else
+        puts 'invalid input'
+      end
+    end
+
     def report
       puts "#{@position_x},#{@position_y},#{@facing}"
     end
