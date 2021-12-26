20f28f6519ebfb2cb4a4c5df1883b0f2a3895c52
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-27 00:20:14 +0330

    add robot instance

--- lib/robots_dilemma.rb
+++ lib/robots_dilemma.rb
@@ -1,9 +1,12 @@
 require "tty-prompt"
 
 require_relative "robots_dilemma/version"
+require_relative "robots_dilemma/robot"
 
 
 module RobotsDilemma
   class Error < StandardError; end
   # Your code goes here...
+  robot = RobotsDilemma::Robot.new
+  robot.report
 end
