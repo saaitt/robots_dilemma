2accb1354ac974aa660a53613e9cfa4b894aadb4
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-31 19:42:06 +0330

    fix integer parsing

--- lib/robots_dilemma/board.rb
+++ lib/robots_dilemma/board.rb
@@ -2,13 +2,13 @@ module RobotsDilemma
   class Board
 
     def initialize
-      @height = ENV['BOARD_HEIGHT'].nil? ? 5 : ENV['BOARD_HEIGHT']
-      @width = ENV['BOARD_WIDTH'].nil? ? 5 : ENV['BOARD_WIDTH']
+      @height = ENV['BOARD_HEIGHT'].nil? ? 5 : ENV['BOARD_HEIGHT'].to_i
+      @width = ENV['BOARD_WIDTH'].nil? ? 5 : ENV['BOARD_WIDTH'].to_i
     end
 
     def check_positions(position)
-      x = position[0]
-      y = position[1]
+      x = position[0].to_i
+      y = position[1].to_i
       facing = position[2]
       if x > 0 && x <= @width
         if y > 0 && y <= @height
@@ -21,32 +21,32 @@ module RobotsDilemma
     end
 
     def can_move(position)
-      x = position[0]
-      y = position[1]
+      x = position[0].to_i
+      y = position[1].to_i
       facing = position[2]
       case facing
       when "NORTH"
         if y < @height
-          true
+          return true
         end
         false
       when "SOUTH"
         if y > 0
-          true
+          return true
         end
         false
       when "EAST"
         if x < @width
-          true
+          return true
         end
         false
       when "WEST"
         if x > 0
-          true
+          return true
         end
         false
       else
-        # type code here
+        false
       end
     end
 
