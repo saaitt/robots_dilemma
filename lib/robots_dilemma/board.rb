05310413ca342d5b4ce15b00e4f86535c4d0872a
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-31 17:02:03 +0330

    add board class

--- lib/robots_dilemma/board.rb
+++ lib/robots_dilemma/board.rb
@@ -1,1 +1,10 @@
+module RobotsDilemma
+  class Board
 
+    def initialize
+      @length = ENV['BOARD_LENGTH'].nil? ? 5 : ENV['BOARD_LENGTH']
+      @width = ENV['BOARD_WIDTH'].nil? ? 5 :ENV['BOARD_WIDTH']
+    end
+
+  end
+end
