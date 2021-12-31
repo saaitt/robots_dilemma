39b04311002d590ca72476f64d4b3c842d8fd482
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-31 17:52:57 +0330

    add place

--- lib/robots_dilemma/operator.rb
+++ lib/robots_dilemma/operator.rb
@@ -6,5 +6,14 @@ module RobotsDilemma
       @robot = RobotsDilemma::Robot.new
     end
 
+    def place(input)
+      unless input[1].nil?
+        position = input[1].split(',')
+        unless @board.check_positions(position)
+          @robot.place(position[0].to_i, position[1].to_i, position[2])
+        end
+      end
+    end
+
   end
 end
