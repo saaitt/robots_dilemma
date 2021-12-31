03e8b18db37956525189241287c107c5f4e27988
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-31 17:21:29 +0330

    change length to height

--- lib/robots_dilemma/board.rb
+++ lib/robots_dilemma/board.rb
@@ -2,7 +2,7 @@ module RobotsDilemma
   class Board
 
     def initialize
-      @length = ENV['BOARD_LENGTH'].nil? ? 5 : ENV['BOARD_LENGTH']
+      @height = ENV['BOARD_HEIGHT'].nil? ? 5 : ENV['BOARD_HEIGHT']
       @width = ENV['BOARD_WIDTH'].nil? ? 5 :ENV['BOARD_WIDTH']
     end
 
