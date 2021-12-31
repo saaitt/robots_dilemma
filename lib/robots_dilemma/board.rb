4b395a9b680d77f360a6e22da41f5c2f215bcb2b
Saeed Firouzi <saeid.firouzi@gmail.com>
2021-12-31 17:46:56 +0330

    add board positions validation

--- lib/robots_dilemma/board.rb
+++ lib/robots_dilemma/board.rb
@@ -6,5 +6,19 @@ module RobotsDilemma
       @width = ENV['BOARD_WIDTH'].nil? ? 5 :ENV['BOARD_WIDTH']
     end
 
+    def check_positions(position)
+      x = position[0]
+      y = position[1]
+      facing = position[2]
+      if x > 0 && x <= @width
+        if y > 0 && y <= @height
+          if facing == "NORTH" || facing == "SOUTH" || facing == "EAST" || facing == "WEST"
+            return true
+          end
+        end
+      end
+      false
+    end
+
   end
 end
