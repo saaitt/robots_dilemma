fec78ee035e4e3ec759aa2975516149c00d25019
Saeed Firouzi <saeid.firouzi@gmail.com>
2022-01-08 02:06:39 +0330

    add board unit tests - can_move

--- test/board_test.rb
+++ test/board_test.rb
@@ -47,5 +47,42 @@ class BoardTest < Minitest::Test
 
     assert_equal true, board.check_positions([valid_x, valid_y, valid_facing]), "board should return true"
   end
+
+  def test_board_doesnt_allow_moving_outside_north
+    board = ::RobotsDilemma::Board.new
+    valid_x = ENV['BOARD_WIDTH'].nil? ? 4 : ENV['BOARD_WIDTH'].to_i - 1
+    border_y = ENV['BOARD_HEIGHT'].nil? ? 5 : ENV['BOARD_HEIGHT'].to_i
+    valid_facing = "NORTH"
+
+    assert_equal false, board.can_move([valid_x, border_y, valid_facing]), "board should return false"
+  end
+
+  def test_board_doesnt_allow_moving_outside_east
+    board = ::RobotsDilemma::Board.new
+    border_x = ENV['BOARD_WIDTH'].nil? ? 5 : ENV['BOARD_WIDTH'].to_i
+    valid_y = ENV['BOARD_HEIGHT'].nil? ? 4 : ENV['BOARD_HEIGHT'].to_i - 1
+    valid_facing = "EAST"
+
+    assert_equal false, board.can_move([border_x, valid_y, valid_facing]), "board should return false"
+  end
+
+  def test_board_doesnt_allow_moving_outside_west
+    board = ::RobotsDilemma::Board.new
+    border_x = 0
+    valid_y = ENV['BOARD_HEIGHT'].nil? ? 4 : ENV['BOARD_HEIGHT'].to_i - 1
+    valid_facing = "WEST"
+
+    assert_equal false, board.can_move([border_x, valid_y, valid_facing]), "board should return false"
+  end
+
+  def test_board_doesnt_allow_moving_outside_south
+    board = ::RobotsDilemma::Board.new
+    valid_x = ENV['BOARD_WIDTH'].nil? ? 4 : ENV['BOARD_WIDTH'].to_i - 1
+    border_y = 0
+    valid_facing = "SOUTH"
+
+    assert_equal false, board.can_move([valid_x, border_y, valid_facing]), "board should return false"
+  end
+
 end
 
