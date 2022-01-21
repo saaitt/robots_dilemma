80ad316fe55f62f78fb654056f119d532451e926
saaitt <saeid.firouzi@gmail.com>
2022-01-22 01:18:10 +0330

    fix robot unit tests

--- test/robot_test.rb
+++ test/robot_test.rb
@@ -1,27 +1,34 @@
 require 'minitest/autorun'
 
 require_relative "../lib/robots_dilemma/robot"
+require_relative "../lib/robots_dilemma/board"
 
 class RobotTest < Minitest::Test
   def test_robot_can_move
+    $board = RobotsDilemma::Board.new
     @robot = ::RobotsDilemma::Robot.new
-    @robot.place(0,0,"NORTH")
+    @robot.place(["PLACE", "0,0,NORTH"])
     @robot.move
-    assert_equal "0,1,NORTH", @robot.report, "robot.report should show a string like '0,1,NORTH' "
+    result = @robot.report
+    assert_equal "0,1,NORTH", "#{result[:x]},#{result[:y]},#{result[:facing]}", "robot.report should show a string like '0,1,NORTH' "
   end
 
   def test_robot_can_turn_right
+    $board = RobotsDilemma::Board.new
     @robot = ::RobotsDilemma::Robot.new
-    @robot.place(0,0,"NORTH")
-    @robot.turn "RIGHT"
-    assert_equal "0,0,EAST", @robot.report, "robot.report should show a string like '0,0,EAST' "
+    @robot.place(["PLACE", "0,0,NORTH"])
+    @robot.right
+    result = @robot.report
+    assert_equal "0,0,EAST", "#{result[:x]},#{result[:y]},#{result[:facing]}", "robot.report should show a string like '0,0,EAST' "
   end
 
   def test_robot_can_turn_left
+    $board = RobotsDilemma::Board.new
     @robot = ::RobotsDilemma::Robot.new
-    @robot.place(0,0,"NORTH")
-    @robot.turn "LEFT"
-    assert_equal "0,0,WEST", @robot.report, "robot.report should show a string like '0,0,WEST' "
+    @robot.place(["PLACE", "0,0,NORTH"])
+    @robot.left
+    result = @robot.report
+    assert_equal "0,0,WEST", "#{result[:x]},#{result[:y]},#{result[:facing]}", "robot.report should show a string like '0,0,WEST' "
   end
 
   def test_robot_cant_move_unless_placed
