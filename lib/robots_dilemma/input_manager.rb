6ae89178133ce9fac330efba854c6354d862a588
saaitt <saeid.firouzi@gmail.com>
2022-01-28 01:56:50 +0330

    add general error handling

--- lib/robots_dilemma/input_manager.rb
+++ lib/robots_dilemma/input_manager.rb
@@ -2,16 +2,16 @@ module RobotsDilemma
   class InputManager
     def process(command_string)
       command_parts = command_string.split(" ")
-      # begin
+      begin
         if command_parts[1].nil?
           result = $robot.public_send(command_parts[0].downcase)
         else
           result = $robot.public_send(command_parts[0].downcase,command_parts)
         end
       result
-      # rescue
-      #   puts "an unexpected thing happened"
-      # end
+      rescue
+        puts "an unexpected thing happened"
+      end
     end
   end
 end
